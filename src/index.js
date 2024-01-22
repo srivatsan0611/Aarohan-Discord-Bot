bbd5b418272a1b4ed19e7d435b72fc7e212a2f09
Srivatsan Srinivasan <100481554+srivatsan0611@users.noreply.github.com>
2024-01-22 19:30:06 +0530

    initial register commands for test

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} is online.`);
+});
+
+client.on('interactionCreate', (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'hey') {
+    return interaction.reply('hey there!');
+  }
+
+  if (interaction.commandName === 'naa') {
+    return interaction.reply('Ready Dhaan varavaa?');
+  }
+});
+
+client.login(process.env.token);
